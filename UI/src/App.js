640e232c5aca90cde7d6fcb4c65f0045c7b2fef4
Marouan Mohamed <marouan.mohamed@bnpparibas-pf.com>
2020-05-22 10:08:27 +0200

    Functional version

--- UI/src/App.js
+++ UI/src/App.js
@@ -2,8 +2,8 @@ import createEngine, { DagreEngine, PathFindingLinkFactory, DefaultNodeModel, Di
 import React from 'react';
 import { CanvasWidget} from '@projectstorm/react-canvas-core';
 import BaseWidget from './BaseWidget'
-import {AdvancedLinkFactory, AdvancedPortModel, AdvancedLinkModel} from './AdvancedLink'
-import {ArrowsLinkFactory, ArrowsPortModel} from './ArrowsLink'
+import {AdvancedLinkFactory, AdvancedPortModel, AdvancedLinkModel} from './link/AdvancedLink'
+import {ArrowsLinkFactory, ArrowsPortModel} from './link/ArrowsLink'
 import { AppNodeFactory } from './node/AppNodeFactory';
 import { AppNodeModel } from './node/AppNodeModel';
 
@@ -15,44 +15,6 @@ function App() {
   engine.getLinkFactories().registerFactory(new ArrowsLinkFactory());
   engine.getNodeFactories().registerFactory(new AppNodeFactory());
 
-  // node 1
-  const node1 = new DefaultNodeModel({
-    name: 'Node 1',
-    color: 'rgb(0,192,255)',
-  });
-  node1.setPosition(100, 100);
-  let port1 = node1.addPort(new AdvancedPortModel(false, 'Out'));
-  // node 2
-  const node2 = new DefaultNodeModel({
-    name: 'Node 2',
-    color: 'rgb(192,255,0)',
-  });
-  node2.setPosition(400, 100);
-  let port2 = node2.addPort(new AdvancedPortModel(true, 'In'));
-  // link them and add a label to the link
-  const link = port1.link(port2);
-  //link.addLabel('Hello World!');
-  
-
-  // node 3
-  const node3 = new DefaultNodeModel({
-    name: 'Node 3',
-    color: 'rgb(0,192,255)',
-  });
-  node3.setPosition(100, 300);
-  let port3 = node3.addPort(new ArrowsPortModel(false, 'Out'));
-
-
-
-
-
-
-
-
-
-
-
-
 
 
   // Product
@@ -60,14 +22,14 @@ function App() {
     name: 'Product View',
     color: 'rgb(192,255,0)',
   });
-  product.setPosition(100, 300);
+  //product.setPosition(100, 300);
   let outProductPortV1 = product.addPort(new AdvancedPortModel(false, 'V1', 'Master*'));
   // details
   let detail = new AppNodeModel({
     name: 'Details Bloc',
     color: 'rgb(192,255,0)',
   });
-  detail.setPosition(400, 150);
+  //detail.setPosition(400, 150);
   let inDetailPort = detail.addPort(new AdvancedPortModel(true, 'IN'));
   let outDetailPortV1 = detail.addPort(new AdvancedPortModel(false, 'V1', 'V1'));
   // Review
@@ -75,7 +37,7 @@ function App() {
     'name': 'Review bloc',
     color: 'rgb(0,192,255)',
   });
-  review.setPosition(400, 350);
+  //review.setPosition(400, 350);
   let inReviewPort = review.addPort(new AdvancedPortModel(true, 'Sidecare'));
   let outReviewPortV1 = review.addPort(new AdvancedPortModel(false, 'V1', 'V1'));
   let outReviewPortV2 = review.addPort(new AdvancedPortModel(false, 'V2', 'V2'));
@@ -85,7 +47,7 @@ function App() {
     'name': 'Rating bloc',
     color: 'rgb(0,192,255)',
   });
-  rating.setPosition(700, 350);
+  //rating.setPosition(700, 350);
   let inRatingPort = rating.addPort(new AdvancedPortModel(true, 'Sidecare'));
   let outRatingPortV1 = rating.addPort(new AdvancedPortModel(false, 'V1', 'V1'));
   let outRatingPortV2 = rating.addPort(new AdvancedPortModel(false, 'V2', 'V2'));
@@ -103,20 +65,21 @@ function App() {
 
   engine.setModel(model);
 
-  /*setTimeout(function(){
+  setTimeout(function(){ // @see https://github.com/dagrejs/dagre/wiki
     const dagre = new DagreEngine({
       graph: {
         rankdir: 'LR',
-        ranker: 'longest-path',
+        ranker: 'network-simplex', // 'tight-tree, longest-path',
         marginx: 100,
-        marginy: 100
+        marginy: 100,
+        ranksep: 100, // horizontal sep
       },
       includeLinks: true
     });
     dagre.redistribute(model);
     engine.getLinkFactories().getFactory(PathFindingLinkFactory.NAME).calculateRoutingMatrix();
     engine.repaintCanvas();
-  }, 5000);*/
+  }, 100);
 
   return (
     <BaseWidget>
--- UI/src/BaseWidget.js
+++ UI/src/BaseWidget.js
@@ -1,8 +1,6 @@
 import * as React from 'react';
 import styled from '@emotion/styled';
 
-
-
 export const Container = styled.div`
 	height: 100%;
 	background-color: ${p => p.background};
